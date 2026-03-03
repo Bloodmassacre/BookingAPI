c5124df2deed91976271ed784b16ddc06a840fde
Jora <Jora@BLOODMASSACRE>
2026-03-03 12:24:28 +0500

    Добавьте файлы проекта.

---
+++ Api/Models/booking.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public int event_id { get; set; }
+        public int user_id { get; set; }
+        public DateTime create_at { get; set; } = DateTime.Now;
+
+    }
+}
